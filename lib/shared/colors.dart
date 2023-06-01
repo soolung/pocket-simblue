d70c26a694e1d00e28ec06422fc8e1f7ae38ca1f
gimhanul <gimhanuly@gmail.com>
2023-06-01 10:40:44 +0900

    ADD :: Palette

---
+++ lib/shared/colors.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static Color primary50 = const Color(0xffFEF3F2);
+  static Color primary100 = const Color(0xffFDE4E3);
+  static Color primary200 = const Color(0xffFCCECC);
+  static Color primary300 = const Color(0xffF9ACA8);
+  static Color primary400 = const Color(0xffF36E67);
+  static Color monoBlack = const Color(0xff000000);
+  static Color monoWhite = const Color(0xffFFFFFF);
+  static Color gray900 = const Color(0xff282828);
+  static Color gray800 = const Color(0xff4B4B4B);
+  static Color gray700 = const Color(0xff5E5E5E);
+  static Color gray600 = const Color(0xff727272);
+  static Color gray500 = const Color(0xff868686);
+  static Color gray400 = const Color(0xffA6A6A6);
+  static Color gray300 = const Color(0xffBBBBBB);
+  static Color gray200 = const Color(0xffD1D1D1);
+  static Color gray100 = const Color(0xffE8E8E8);
+  static Color gray50 = const Color(0xffF3F3F3);
+  static Color green50 = const Color(0xffEAF6ED);
+  static Color green100 = const Color(0xffD3EFDA);
+  static Color green200 = const Color(0xffA6DFB7);
+  static Color green300 = const Color(0xff7FD99A);
+  static Color green400 = const Color(0xff06C167);
+  static Color green500 = const Color(0xff009A51);
+  static Color red50 = const Color(0xffFFF0EE);
+  static Color red100 = const Color(0xffFFE1DE);
+  static Color red200 = const Color(0xffFFC2BC);
+  static Color red300 = const Color(0xffFFB2AB);
+  static Color red400 = const Color(0xffFC7F79);
+  static Color red500 = const Color(0xffF83445);
+}
